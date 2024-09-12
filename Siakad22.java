3aa60d7aec6339a9d41e5974a0787134f1c8b250
nevita <triyanevita7@gmail.com>
2024-09-12 10:41:30 +0700

    jobshett3

--- Siakad22.java
+++ Siakad22.java
@@ -6,7 +6,7 @@ public class Siakad22 {
     String nama, nim;
     char kelas;
     byte absen;
-    double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+    double nilaiKuis, nilaiTugas, nilaiUts, nilaiUas, nilaiAkhir;
     System.out.print("Masukkan nama: ");
     nama = sc.nextLine();
     System.out.print("Masukkan NIM: ");
@@ -19,9 +19,13 @@ public class Siakad22 {
     nilaiKuis = sc.nextDouble();
     System.out.print("Masukkan nilai tugas: ");
     nilaiTugas = sc.nextDouble();
-    System.out.print("Masukkan nilai ujian: ");
-    nilaiUjian = sc.nextDouble();
-    nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian)/ 3;
+    System.out.print("Masukkan nilai UTS: ");
+    nilaiUts = sc.nextDouble();
+    System.out.print("Masukkan nilai UAS: ");
+    nilaiUas = sc.nextDouble();
+
+    nilaiAkhir = ((nilaiKuis * 0.20) + (nilaiTugas * 0.15) + (nilaiUts * 0.30) + (nilaiUas * 0.35))/ 4;
+
     System.out.println("Nama: " + nama + "NIM: " + nim);
     System.out.println("Kelas: " + kelas + "Absen: " + absen);
     System.out.println("Nilai Akhir: " + nilaiAkhir);
