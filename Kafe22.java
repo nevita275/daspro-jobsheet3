2c7b03290cdbe0e4327ba27f0d135d1968538454
nevita <triyanevita7@gmail.com>
2024-09-12 12:33:44 +0700

    jobsheet3

--- Kafe22.java
+++ Kafe22.java
@@ -5,7 +5,7 @@ public class Kafe22 {
         boolean keanggotaan;
         int jmlKopi, jmlTeh, jmlRoti;
         double hargaKopi = 12000.0, hargaTeh = 7000.0, hargaRoti = 20000.0;
-        float diskon = 10 / 100f;
+        float diskon = 10 / 100;
         System.out.print("Masukkan keanggotaan (true/false): ");
         keanggotaan = input.nextBoolean();
         System.out.print("Masukkan jumlah pembelian kopi: ");
@@ -14,11 +14,15 @@ public class Kafe22 {
         jmlTeh = input.nextInt();
         System.out.print("Masukkan jumlah pembelian roti: ");
         jmlRoti = input.nextInt();
+
         double totalHarga = (jmlKopi * hargaKopi) + (jmlTeh *hargaTeh) + (jmlRoti * hargaRoti);
         double nominalBayar = totalHarga - (diskon * totalHarga);
+        byet totalbyet = (byet) totalHarga;
+        int nominallnt = (int) nominalBayar;
+     
         System.out.println("Keanggotaan pelanggan " + keanggotaan);
         System.out.println("Item pembelian " + jmlKopi + " kopi, " + jmlTeh + " teh, " + jmlRoti + " roti");
         System.out.println("Nominal bayar Rp " + nominalBayar);
-
+        System.out.println(nominallnt);
     }
 }
